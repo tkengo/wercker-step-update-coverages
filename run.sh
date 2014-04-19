109fdc78290162998aad06df1a5f93ea13590f93
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-04-20 08:43:10 +0900

    Added run.sh that is executed by wercker

---
+++ run.sh
@@ -0,0 +1,17 @@
+git clone -b gh-pages git@github.com:tkengo/hometype.git
+
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
+
+mv coverage hometype/.
+cd hometype
+
+diff=$(git status -s | grep coverage)
+if [ -z $diff ]; then
+    echo 'Skip gh-pages step because the coverage was not updated.'
+    exit 0
+fi
+
+git add .
+git commit -m "Updated code coverage: `date +'%Y-%m-%d%k:%M:%S'`"
+git push origin gh-pages
